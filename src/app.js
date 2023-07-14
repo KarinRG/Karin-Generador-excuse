28d083a8fb0bd4f410dcfc399d8f719037631870
KarinRG <131704279+KarinRG@users.noreply.github.com>
2023-07-14 01:08:21 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -5,9 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
+let who = [" The dog ", " My grandma ", " His turtle ", " My bird "];
+let action = [" ate ", " peed ", " crushed ", " broke "];
+
+let what = [" my homework ", " the keys ", " the car "];
 let when = [
   "before the class",
   "right on time",
